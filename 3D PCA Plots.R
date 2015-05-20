68dd2ea5b7ee3be18e451b4c3d50d5235ad22672
Heather Gibling <heathergibling@gmail.com>
2015-05-20 16:13:47 -0400

    added plot code for preprocessed data with annotated/filtered genes

--- 3D PCA Plots.R
+++ 3D PCA Plots.R
@@ -1,5 +1,6 @@
 library(pca3d)
 
-raw.3d <- pca3d(pca.values.raw$x, components=1:3, col=pca.colors, show.plane=T, show.labels=T)
-pre.3d <- pca3d(pca.values.preprocessed$x, components=1:3, col=pca.colors, show.plane=T, show.labels=T)
+raw.3d <- pca3d(pca.values.raw$x, components=1:3, col=plot.colors, show.plane=T, show.labels=T)
+pre.3d <- pca3d(pca.values.preprocessed$x, components=1:3, col=plot.colors, show.plane=T, show.labels=T)
 
+ann.3d <- pca3d(pca.annotated.genes$x, components=1:3, col=plot.colors, show.plane=T, show.labels=T)

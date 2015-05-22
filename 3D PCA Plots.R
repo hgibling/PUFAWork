c0161f5c91c05e3a0310348afb8d514e22f0c3ad
Heather Gibling <heathergibling@gmail.com>
2015-05-22 12:03:02 -0400

    organized files

--- 3D PCA Plots.R
+++
@@ -1,6 +0,0 @@
-library(pca3d)
-
-raw.3d <- pca3d(pca.values.raw$x, components=1:3, col=plot.colors, show.plane=T, show.labels=T)
-pre.3d <- pca3d(pca.values.preprocessed$x, components=1:3, col=plot.colors, show.plane=T, show.labels=T)
-
-ann.3d <- pca3d(pca.annotated.genes$x, components=1:3, col=plot.colors, show.plane=T, show.labels=T)
--- OtherScripts/getfasta.sh
+++
@@ -1,1 +0,0 @@
-echo -e "24296" | while read G; do curl -s "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/elink.fcgi?dbfrom=gene&db=protein&id=${G}" | grep -A 1 "<Link>" | grep "<Id>" | cut -d '>' -f 2 | cut -d '<' -f 1 | while read S ; do curl -s "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=protein&id=${S}&retmode=text&rettype=fasta" ; done;  done

6b37b0d3496dbef4b02e37ca5e6c8ae9fbe9a18b
Heather Gibling <heathergibling@gmail.com>
2015-05-22 13:25:57 -0400

    script determines longest protein from list of fasta files and gets the protein id

--- OtherScripts/proteinlength.py
+++ OtherScripts/proteinlength.py
@@ -1,6 +1,16 @@
+lengths = dict()
 with open('fastainfo.txt') as f:
     for line in f:
         if line.startswith('>'):
+            #line.rstrip()
             lengthpos = line.rfind(':')
-            print line[lengthpos+1:]
-        
+            aalength = line[lengthpos+1:]
+            aalength = aalength.rstrip()
+            descpos1 = line.find('|')
+            newdesc = line[descpos1+1:]
+            descpos2 = newdesc.find('|')
+            desc = newdesc[:descpos2]
+            lengths[desc] = aalength
+   
+maxID = max(lengths, key=lengths.get)
+print maxID

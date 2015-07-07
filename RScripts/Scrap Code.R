e57b632a9d9ef8138fabd84070c3fc72ac1c7d08
Heather Gibling <heathergibling@gmail.com>
2015-07-07 11:12:32 -0400

    more code

--- RScripts/Scrap Code.R
+++ RScripts/Scrap Code.R
@@ -17,3 +17,28 @@ hist.colors <- c(black2, "red", black3, "blue", black3, "green", black3, "orange
 dev.off()
 
 ?RLE
+
+
+
+library(dplyr)
+
+agilent.de <- read.csv("~/Desktop/rat de genes.csv", header=F)
+colnames(agilent.de) <- c("DE.Genes")
+
+agilent.annot <- read.table("~/Desktop/agilent rat annotations simple.txt", header=T, sep="\t", na.strings="")
+
+pos <- agilent.annot[,1] %in% agilent.de[,1]
+agilent.just <- agilent.annot[which(pos==T),]
+
+agilent <- merge(agilent.de, agilent.just, by.x="DE.Genes", by.y="PrimaryAccession")
+
+agilent <- na.omit(agilent)
+agilentu <- unique(agilent)
+
+agilent.de.genes<- agilent[,2]
+
+my.de <- read.table("~/Desktop/PUFA Microarray Analysis/All 32 Arrays/Preprocessed Data/Filtered Genes Data/Differentially Expressed Gene Lists/ANOVA DE Genes.txt", header=F)
+
+my.de <- my.de[,1]
+de.pos <- my.de %in% agilent.de.genes
+shared.de <- my.de[which(de.pos==T)]

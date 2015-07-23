b499e98c0cc21b4f48fdf7f1c056770a6a9c0575
Heather Gibling <heathergibling@gmail.com>
2015-07-23 16:05:34 -0400

    Added code for retrieving 18/25 missing pathways

--- RScripts/GSEA.R
+++ RScripts/GSEA.R
@@ -1,6 +1,7 @@
 load("~/Desktop/SummerWork/PUFA All.Rdata")
 library(gage)
 library(stringr)
+library(KEGGREST)
 
 
 ### Generate Gene Sets for Rat KEGG Pathways
@@ -18,6 +19,24 @@ split.names <- str_split(set.names, " ", n=2)
 
 for (i in 1:length(set.names)){
 	mat <- matrix(nrow=1, c(split.names[[i]][2], split.names[[i]][1], kegg.sets[[i]]))
+	write.table(mat, "Rat KEGG Sets.gmt", sep="\t", quote=F, row.names=F, col.names=F, append=T)
+}
+
+
+## Get Missing KEGG Pathways
+
+# Manually compared list of Rat KEGG Pathway IDs obtained above to list on KEGG website: 25 pathways missing. 7 pathways are not able to have genes retrieved from keggGet--need to figure out solution
+
+missing <- c("rno00220", "rno04014", "rno04015", "rno04022", "rno04024", "rno04068", "rno04071", "rno04152", "rno04261", "rno04550", "rno04611", "rno04750", "rno04919", "rno04921", "rno04922", "rno04923", "rno05230", "rno05231")
+
+for (i in 1:length(missing)){
+	pathway.info <- keggGet(missing[i])
+	full.name <- pathway.info[[1]]$NAME
+	name <- gsub(" -.*", "", full.name)
+	full.genes <- pathway.info[[1]]$GENE
+	get <- seq(1, length(full.genes), 2)
+	genes <- full.genes[get]
+	mat <- matrix(nrow=1, c(name, missing[i], genes))
 	write.table(mat, "Rat KEGG Sets.gmt", sep="\t", quote=F, row.names=F, col.names=F, append=T)
 }
 
